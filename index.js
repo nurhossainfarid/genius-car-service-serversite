a71fe4885442afa01b4b4d09739ae1fc00fdb311
nurhossainfarid <faahsan1516@gmail.com>
2022-04-27 13:29:48 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.keyuy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const serviceCollection = client.db('geniusCarServices').collection('Service');
+
+        // multiple service
+        app.get('/service', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+
+        // single service
+        app.get('/service/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        })
+
+        // add new service 
+        app.post('/service', async (req, res) => {
+            const newService = req.body;
+            const result = await serviceCollection.insertOne(newService);
+            res.send(result);
+        })
+    }
+    finally {
+        
+    }
+}
+
+run().catch(console.dir)
+
+
+app.get('/', (req, res) => {
+    res.send('Running genius car with mongodb');
+});
+
+app.listen(port, () => {
+    console.log('Mongodb server site running');
+})
