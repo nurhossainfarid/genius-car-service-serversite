0c311f231186d0a8ae08e7eef570e15236e71160
nurhossainfarid <faahsan1516@gmail.com>
2022-05-01 22:43:35 +0600

    change something with heroku

---
+++ _steps-deploy.js
@@ -0,0 +1,12 @@
+/* 
+*One time with heroku 
+1) create a account in heroku 
+2) verify email
+3) install heroku cli
+4) heroku login
+----------------------------------- one time for your computer
+*each project
+1) heroku cerate
+2) 
+
+*/
--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+var jwt = require('jsonwebtoken');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
-app.use(express());
+app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.keyuy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -17,6 +18,16 @@ async function run() {
     try {
         await client.connect();
         const serviceCollection = client.db('geniusCarServices').collection('Service');
+        const orderCollection = client.db('orderCollection').collection('order');
+
+        // AUTH
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            });
+            res.send({ accessToken });
+        })
 
         // multiple service
         app.get('/service', async (req, res) => {
@@ -34,10 +45,36 @@ async function run() {
             res.send(service);
         })
 
+
         // add new service 
         app.post('/service', async (req, res) => {
             const newService = req.body;
             const result = await serviceCollection.insertOne(newService);
+            res.send(result);
+        })
+
+        // Delete service
+        app.delete('/service/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await serviceCollection.deleteOne(query);
+            res.send(result);
+        })
+
+        // order collection api
+
+        // order history
+        app.get('/order', async (req, res) => {
+            const email = req.query.email;
+            const query = {email: email};
+            const cursor = orderCollection.find(query);
+            const orders = await cursor.toArray();
+            res.send(orders)
+        })
+
+        app.post('/order', async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
             res.send(result);
         })
     }
